7fa15a575c86c01b00e4798f19747efb19632fc1
Dominique Skaf <doskaf@yahoo.com>
2019-09-18 02:24:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,7 @@ class School
   def add_student(name, grade)
     if @roster.none?(grade)
       @roster[grade] = []
-      @roster[grade] << name
-    elsif @roster.include?(grade)
-    @roster[grade] << name
     end
+      @roster[grade] << name
   end
 end
